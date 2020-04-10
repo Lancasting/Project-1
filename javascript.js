ebea00c97f25b176dda0cd1749171673e5ceef2b
jordan o'neal <joneal6694@gmail.com>
2020-04-10 10:47:01 -0600

    more styling. updated JS for functioning stormglass API call and added results and previous search divs

--- javascript.js
+++ javascript.js
@@ -1,24 +1,40 @@
 // Sams stormglass API key 8aca6f50-7acf-11ea-98e7-0242ac130002-8aca6ff0-7acf-11ea-98e7-0242ac130002
 // Sams Google API key AIzaSyA60E-7YODoqYm16Aanxh8PoR4OXJugr_U
-$(document).ready(function(){
+// $(document).ready(function(){
 
-var queryURLStorm = "https://api.stormglass.io/v2/weather/point";
-var queryURLGoogle = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
-var searchresults = $()
-$("search-button").on(click, function() {
-$.ajax({
-    url: queryURLStorm, queryURLGoogle,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  })
-})
+// var queryURLStorm = "https://api.stormglass.io/v2/weather/point";
+// var queryURLGoogle = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
+// var searchresults = $()
+// $("search-button").on(click, function() {
+// $.ajax({
+//     url: queryURLStorm, queryURLGoogle,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   })
+// })
 
-}) 
-
-
+// }) 
 
 
+// This fetches the data from our storm glass API
+var APIKey="4470429a-793b-11ea-98e7-0242ac130002-4470434e-793b-11ea-98e7-0242ac130002"
 
+const lat = 45.7;
+const lng = -6.0;
+const params = [
+  'secondarySwellDirection', 'secondarySwellHeight', 'secondarySwellPeriod',
+  'windWaveDirection', 'windWaveHeight', 'windWavePeriod',
+  'swellDirection', 'swellHeight', 'swellPeriod',
+  'waveDirection', 'waveHeight', 'wavePeriod',
+  'windDirection', 'windSpeed', 'gust',
+  'waterTemperature',
+].join(',');
 
-
+fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
+  headers: {
+    'Authorization': APIKey
+  }
+}).then((response) => response.json()).then((jsonData) => {
+  console.log(jsonData); //displays object data in console
+});
