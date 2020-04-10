206c036babababebdbee36c09816c40d03cb4204
Samuel Lancaster <samuellancaster66@gmail.com>
2020-04-09 21:33:40 -0600

    added information in Javasctipt and readme

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// Sams stormglass API key 8aca6f50-7acf-11ea-98e7-0242ac130002-8aca6ff0-7acf-11ea-98e7-0242ac130002
+// Sams Google API key AIzaSyA60E-7YODoqYm16Aanxh8PoR4OXJugr_U
+$(document).ready(function(){
+
+var queryURLStorm = "https://api.stormglass.io/v2/weather/point";
+var queryURLGoogle = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
+var searchresults = $()
+$("search-button").on(click); {
+$.ajax({
+    url: queryURLStorm, queryURLGoogle,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  })
+}
+
+}) 
+
+
+
+
+
+
