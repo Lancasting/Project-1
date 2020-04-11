d6429d045186dbae76ea57d975f7aeeaf769cff9
Samuel Lancaster <samuellancaster66@gmail.com>
2020-04-11 13:24:32 -0600

    adding Google maps API

--- javascript.js
+++ javascript.js
@@ -32,7 +32,7 @@ var queryURLOpenWeather = "https://api.openweathermap.org/data/2.5/weather?q=" +
 $.ajax({
   url: queryURLOpenWeather,
   method: "GET"
-}).then(function(response){
+}).then(function (response) {
   console.log(response);
   console.log(response.coord.lon)
   console.log(response.coord.lat);;
@@ -47,7 +47,7 @@ $.ajax({
 
 
 // This fetches the data from our storm glass API
-var APIKey="4470429a-793b-11ea-98e7-0242ac130002-4470434e-793b-11ea-98e7-0242ac130002"
+var APIKey = "8aca6f50-7acf-11ea-98e7-0242ac130002-8aca6ff0-7acf-11ea-98e7-0242ac130002"
 
 const lat = 45.7;
 const lng = -6.0;
@@ -65,13 +65,52 @@ fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=$
     'Authorization': APIKey
   }
 }).then((response) => response.json()).then((jsonData) => {
+  let fiveDay = {}
+  for (let i = 0; i < jsonData.hours.length; i++) {
+    // use for 6 hour or 12 or 24
+    let dateArr = jsonData.hours[i].time.split('T')
+    console.log(dateArr);
+    if (!fiveDay[dateArr[0]]) {
+      fiveDay[dateArr[0]] = jsonData.hours[i];
+    }
+  }
+  console.log(fiveDay)
   console.log(jsonData); //displays object data in console
-  
+
 });
 
-function initMap() {
-  var uluru = {lat: -25.344, lng: 131.036}
-  var map = new google.maps.Map(
-  document.getElementById('map'), {zoom: 4, center: uluru});
-  var marker = new google.maps.Marker({position: uluru, map: map});
+function newfunction(lat,lng) {
+
 }
+
+      function initMap() {
+        var myLatlng = {lat: -25.363, lng: 131.044};
+
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: myLatlng});
+
+        // Create the initial InfoWindow.
+        var infoWindow = new google.maps.InfoWindow(
+            {content: 'Click the map to get Lat/Lng!', position: myLatlng});
+        infoWindow.open(map);
+
+        // Configure the click listener.
+        map.addListener('click', function(mapsMouseEvent) {
+          // Close the current InfoWindow.
+          infoWindow.close();
+
+          // Create a new InfoWindow.
+          infoWindow = new google.maps.InfoWindow({position: mapsMouseEvent.latLng});
+          infoWindow.setContent(mapsMouseEvent.latLng.toString());
+          infoWindow.open(map);
+          var coords = mapsMouseEvent.latLng.toString().substring(1);
+          var coordsArr = coords.substring(0, coords.length - 1).split(', ');
+          console.log(coordsArr);
+          var lat = coordsArr[0];
+          var lng = coordsArr[1];
+
+// make api call here to take in lat lng
+newfunction(lat,lng)
+        });
+      }
+
